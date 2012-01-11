61c918961b07e515d4932a2ddf8b2d4f750b00ec
Adrian <akuri88@gmail.com>
2012-01-10 23:30:36 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datafut::Application.config.secret_token = '692991b765d9573cedc08b802a598a2a795c143a867c77e741e52fa341a7b0fd11012bdcf698606d82867e06af0bae0283d75ac2a63ffa26f9c32b86f684b39a'
