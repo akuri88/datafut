86e196751503d286ba8f5ad7703365a7b5fb48f0
Adrian <akuri88@gmail.com>
2012-01-17 18:33:22 -0600

    Improved table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
 	
 	def home
 		@equipos = Plantilla__c.query("Torneo__r.Name = 'Clausura 2012' ORDER BY Puntos__c DESC, Diferencia__c DESC, Goles_A_Favor__c DESC, Goles_En_Contra__c DESC, Name ASC")
+		@posicion = 1;
 	end
 
 end
