181918c729d97a3f8f3913720569c32acc468a1b
Adrian <akuri88@gmail.com>
2012-01-21 10:50:48 -0600

    Added Players to Team Page

--- app/controllers/equipos_controller.rb
+++ app/controllers/equipos_controller.rb
@@ -3,5 +3,8 @@ class EquiposController < ApplicationController
 	
 	def show
 		@equipo = Plantilla__c.find(params[:id])
+		@jugadores = Registro__c.query("Plantilla__c = '#{params[:id]}' ORDER BY Portero__c DESC, Defensa__c DESC, Medio__c DESC, Delantero__c DESC")
+		# @resumen = client.query("SELECT Id FROM Resumen_Partido__c WHERE Equipo__c = '#{params[:id]}'")
+		# @partidos = Partido__c.query("Local__c IN '#{@resumen.Id}' OR Visita__c IN '#{@resumen.Id}' ORDER BY Fecha__c")
 	end
 end
